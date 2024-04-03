59d2b04bccd8971b7aeb37c6d72c37061fafa0b7
unknown <jorgeeduardoaguilerapantoja@gmail.com>
2024-04-03 10:24:56 -0400

    Actividad 3

---
+++ Actividad_3_JORGE/Actividad.VISTA/ClienteVista/ClienteInterfaz.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Actividad.VISTA.ClienteVista
+{
+    partial class ClienteInterfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            button5 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = SystemColors.ControlLightLight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(134, 128);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(657, 250);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.Click += dataGridView1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(156, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(537, 18);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(238, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(94, 60);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(169, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(368, 62);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(188, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(677, 62);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(205, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Myanmar Text", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(234, 160, 54);
+            label1.Location = new Point(85, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 24);
+            label1.TabIndex = 6;
+            label1.Text = "NOMBRE";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(157, 13, 20);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.FromArgb(234, 160, 54);
+            button1.Location = new Point(134, 401);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 48);
+            button1.TabIndex = 11;
+            button1.Text = "AGREGAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(157, 13, 20);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.FromArgb(234, 160, 54);
+            button2.Location = new Point(575, 401);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 48);
+            button2.TabIndex = 12;
+            button2.Text = "EDITAR";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(157, 13, 20);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.FromArgb(234, 160, 54);
+            button3.Location = new Point(278, 401);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 48);
+            button3.TabIndex = 13;
+            button3.Text = "ELIMINAR";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(157, 13, 20);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.FromArgb(234, 160, 54);
+            button4.Location = new Point(428, 401);
+            button4.Name = "button4";
+            button4.Size = new Size(116, 48);
+            button4.TabIndex = 14;
+            button4.Text = "DATOS";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(157, 13, 20);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox4);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(918, 112);
+            panel1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Myanmar Text", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(234, 160, 54);
+            label5.Location = new Point(592, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(79, 24);
+            label5.TabIndex = 10;
+            label5.Text = "DIRECCIÓN";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Myanmar Text", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(234, 160, 54);
+            label4.Location = new Point(301, 61);
+            label4.Name = "label4";
+            label4.Size = new Size(61, 24);
+            label4.TabIndex = 9;
+            label4.Text = "CORREO";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Myanmar Text", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(234, 160, 54);
+            label3.Location = new Point(12, 59);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 24);
+            label3.TabIndex = 8;
+            label3.Text = "TELEFONO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Myanmar Text", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(234, 160, 54);
+            label2.Location = new Point(459, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 24);
+            label2.TabIndex = 7;
+            label2.Text = "APELLIDO";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(157, 13, 20);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Tahoma", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.FromArgb(234, 160, 54);
+            button5.Location = new Point(709, 401);
+            button5.Name = "button5";
+            button5.Size = new Size(116, 48);
+            button5.TabIndex = 16;
+            button5.Text = "CERRAR";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.caroo1;
+            pictureBox1.Location = new Point(1, 355);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(127, 110);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            // 
+            // ClienteInterfaz
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 160, 54);
+            ClientSize = new Size(910, 477);
+            Controls.Add(pictureBox1);
+            Controls.Add(button5);
+            Controls.Add(panel1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "ClienteInterfaz";
+            Text = "ClienteInterfaz";
+            Load += ClienteInterfaz_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Panel panel1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button5;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Actividad_3_JORGE/Actividad.VISTA/PedidoVista/PedidoInterfaz.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Actividad.VISTA.PedidoVista
+{
+    partial class PedidoInterfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            dateTimePicker1 = new DateTimePicker();
+            button4 = new Button();
+            colorDialog1 = new ColorDialog();
+            button5 = new Button();
+            panel1 = new Panel();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(157, 13, 20);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.FromArgb(234, 160, 54);
+            button3.Location = new Point(405, 432);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 48);
+            button3.TabIndex = 27;
+            button3.Text = "ELIMINAR";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(157, 13, 20);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Symbol", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.FromArgb(234, 160, 54);
+            button2.Location = new Point(240, 432);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 48);
+            button2.TabIndex = 26;
+            button2.Text = "EDITAR";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(157, 13, 20);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.FromArgb(234, 160, 54);
+            button1.Location = new Point(572, 432);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 48);
+            button1.TabIndex = 25;
+            button1.Text = "AGREGAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(157, 13, 20);
+            label3.Font = new Font("Gill Sans MT", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(637, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 21);
+            label3.TabIndex = 22;
+            label3.Text = "FECHA";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(157, 13, 20);
+            label2.Font = new Font("Gill Sans MT", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(397, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 21);
+            label2.TabIndex = 21;
+            label2.Text = "TOTAL";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(157, 13, 20);
+            label1.Font = new Font("Gill Sans MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(38, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 18);
+            label1.TabIndex = 20;
+            label1.Text = "IDCLIENTE";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(464, 42);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(141, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(240, 115);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(615, 311);
+            dataGridView1.TabIndex = 14;
+            dataGridView1.Click += dataGridView1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(705, 39);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 28;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(234, 160, 54);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Gill Sans MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.FromArgb(157, 13, 20);
+            button4.Location = new Point(171, 76);
+            button4.Name = "button4";
+            button4.Size = new Size(136, 32);
+            button4.TabIndex = 29;
+            button4.Text = "SELECCIONAR";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(157, 13, 20);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.FromArgb(234, 160, 54);
+            button5.Location = new Point(739, 432);
+            button5.Name = "button5";
+            button5.Size = new Size(116, 48);
+            button5.TabIndex = 30;
+            button5.Text = "SALIR";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(157, 13, 20);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(-3, -5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1015, 114);
+            panel1.TabIndex = 31;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.fecca;
+            pictureBox3.Location = new Point(637, 11);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(65, 57);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 32;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.dinero;
+            pictureBox2.Location = new Point(397, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(64, 59);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 31;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.CLIENTE;
+            pictureBox1.Location = new Point(44, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 59);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 30;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.caroo;
+            pictureBox4.Location = new Point(23, 403);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(180, 149);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 32;
+            pictureBox4.TabStop = false;
+            // 
+            // PedidoInterfaz
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 160, 54);
+            ClientSize = new Size(991, 564);
+            Controls.Add(button5);
+            Controls.Add(textBox2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox4);
+            Name = "PedidoInterfaz";
+            Text = "PedidoInterfaz";
+            Load += PedidoInterfaz_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private DateTimePicker dateTimePicker1;
+        private Button button4;
+        private ColorDialog colorDialog1;
+        private Button button5;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+    }
+}
